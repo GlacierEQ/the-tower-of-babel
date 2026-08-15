3c111f0cd7c24c7b0f4ae6a84385b4bfc535d51a
casey <GLACIER.EQUILIBRIUM@GMAIL.COM>
2026-08-14 15:57:34 -1000

    Activate Tower execution path in the main runtime

--- src/tower/__init__.py
+++ src/tower/__init__.py
@@ -1,3 +1,4 @@
-"""The Tower of Babel governance and execution package."""
-
-__version__ = "1.1.0"
+"""The Tower of Babel governance, activation, and execution package."""
+from .activation import ActivationDecision, ActivationMode, activate_execution, activation_surface, resolve_activation
+__version__ = "1.2.1"
+__all__ = ["ActivationDecision", "ActivationMode", "activate_execution", "activation_surface", "resolve_activation"]
